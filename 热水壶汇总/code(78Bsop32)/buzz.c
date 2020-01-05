4795bb3d34d3ecc8c5454200334bc71d13a4775e
Mast <>
2020-01-05 15:14:32 +0800

    热水壶汇总

---
+++ 热水壶汇总/code(78Bsop32)/buzz.c
@@ -0,0 +1,17 @@
+#define __buzz_c
+
+#include  "includeAll.h"
+#include  "buzz.h"
+
+void BuzzCount(uint8_t count) {
+    buzzCounter = count; 
+}
+
+void Buzz() {
+    buzzLastTimer = D_buzzLastTime;
+}
+
+//´ò¿ª·äÃùÆ÷
+void Buzz_ON() {
+		P_buzz=0;
+}
